38a4264dd6fe5664a7f3c842d6ae0244f87c6d90
Huiling Loh <huiling_loh_97@hotmail.com>
2021-11-24 14:48:12 +0800

    created model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  Flat.create!({
+    name: Faker::JapaneseMedia::OnePiece.location,
+    address: Faker::Address.street_address,
+    description: Faker::Games::Dota.quote,
+    price_per_night: rand(75..150),
+    number_of_guests: rand(1..5)
+  })
+end
